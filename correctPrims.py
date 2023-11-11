2d992ce7eb6af2c397faffbe069f535188697527
40258633 <jdonnelly62@qub.ac.uk>
2023-11-11 21:44:54 +0000

    Updated

--- correctPrims.py
+++ correctPrims.py
@@ -1,15 +1,18 @@
+#####################################################################################
+# Implementation for Prim's algorithm MST based on PLK Chapter 8 - Graph Algorithms #
+#####################################################################################
+
+
 def prim_minimum_spanning_tree(graph):
     V, E, W = graph
     Te = set()  # Set of edges in the minimum spanning tree
     Tv = set()  # Set of visited vertices
     u = next(iter(V))  # Starting vertex, choosing any vertex in V
-    L = {}
+    L = {}      # Dictionary for L values of each edge
 
     Tv.add(u)           #Adding initial vertex to Tv
 
     # Initialize L(v) for all vertices
-    #L = {v: float('inf') for v in V}
-
     for v in V - Tv:
         if (u, v) in E:
             L[v] = W[(u, v)]
@@ -45,7 +48,7 @@ def prim_minimum_spanning_tree(graph):
         print(f"\nAdded edge {e} to the minimum spanning tree.")
         print("Current minimum spanning tree edges:", Te)
 
-        # Update L(v) for v ∈ (V − Tv) if there is an edge (w, v) in E with weight less than L(v)
+        # Update L(v) for v ∈ (V − Tv) if there is an edge (w, v) or (v, w) in E with weight less than L(v)
         for v in (V - Tv):
             if (w, v) in E and W[(w, v)] < L[v]:
                 L[v] = W[(w, v)]
@@ -56,22 +59,18 @@ def prim_minimum_spanning_tree(graph):
 
     return Te
 
-#Simple graph
+# Simple graph
 #V = {'A', 'B', 'C', 'D'}
 #E = {('A', 'B'), ('A', 'C'), ('B', 'C'), ('B', 'D'), ('C', 'D')}
 #W = {('A', 'B'): 1, ('A', 'C'): 3, ('B', 'C'): 2, ('B', 'D'): 4, ('C', 'D'): 5}
 
-# Example usage:
-V = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'}
-E = {('A', 'B'), ('A', 'C'), ('B', 'C'), ('B', 'D'), ('C', 'D'), ('D', 'E'), ('E', 'F'), ('E', 'G'), ('F', 'G'), ('G', 'H')}
-W = {('A', 'B'): 4, ('A', 'C'): 2, ('B', 'C'): 5, ('B', 'D'): 10, ('C', 'D'): 3, ('D', 'E'): 7, ('E', 'F'): 1, ('E', 'G'): 8, ('F', 'G'): 6, ('G', 'H'): 9}
-
+V = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K'}
+E = {('A', 'B'), ('A', 'C'), ('B', 'D'), ('C', 'E'), ('D', 'F'), ('E', 'F'), ('F', 'G'), ('G', 'H'), ('H', 'I'), ('I', 'J'), ('J', 'K'), ('K', 'A'), ('B', 'C'), ('C', 'D'), ('D', 'E'), ('E', 'G'), ('G', 'I'), ('I', 'K'), ('K', 'B'), ('F', 'H')}
+W = {('A', 'B'): 5, ('A', 'C'): 3, ('B', 'D'): 6, ('C', 'E'): 2, ('D', 'F'): 9, ('E', 'F'): 4, ('F', 'G'): 7, ('G', 'H'): 1, ('H', 'I'): 5, ('I', 'J'): 10, ('J', 'K'): 3, ('K', 'A'): 8, ('B', 'C'): 4, ('C', 'D'): 1, ('D', 'E'): 7, ('E', 'G'): 5, ('G', 'I'): 2, ('I', 'K'): 6, ('K', 'B'): 9, ('F', 'H'): 3}
 
 graph = (V, E, W)
+
 minimum_spanning_tree = prim_minimum_spanning_tree(graph)
 print("\nFinal Minimum Spanning Tree:", minimum_spanning_tree)
-
-
-
 
 
